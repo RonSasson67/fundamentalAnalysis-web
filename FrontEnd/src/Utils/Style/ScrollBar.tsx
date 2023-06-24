f6e8f2fc03189979a6ade65024cf8e527522c241
RonSasson67 <ron6764527@gmail.com>
2023-06-24 20:32:50 +0300

    Update scroll bar css

--- FrontEnd/src/Utils/Style/ScrollBar.tsx
+++ FrontEnd/src/Utils/Style/ScrollBar.tsx
@@ -4,14 +4,13 @@ const ScrollBar = makeStyles({
   scrollBox: {
     overflowY: "auto", // Enable vertical scrolling
     "&::-webkit-scrollbar": {
-      width: "8px", // Set the width of the scrollbar
+      width: "4px", // Set the width of the scrollbar
     },
     "&::-webkit-scrollbar-track": {
       backgroundColor: "#f1f1f1", // Set the background color of the scrollbar track
     },
     "&::-webkit-scrollbar-thumb": {
-      backgroundColor: "#888", // Set the color of the scrollbar thumb
-      borderRadius: "4px", // Set the radius of the scrollbar thumb
+      backgroundColor: "#1B1B1B", // Set the color of the scrollbar thumb
     },
   },
 });
