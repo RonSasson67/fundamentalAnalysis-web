fa27707f72c10edd4ea804043983c1d828a7971c
ron sason <yafagame@gmail.com>
2023-07-06 00:16:27 +0300

    Save

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,3 +1,4 @@
+import { CssBaseline } from "@mui/material";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { observer } from "mobx-react";
 import { QueryClient, QueryClientProvider } from "react-query";
@@ -7,8 +8,8 @@ import MetricsType from "./Entity/MetricsType";
 import RatioType from "./Entity/RatioType";
 import InputSymbolPage from "./Pages/InputSymbolPage/InputSymbolPage";
 import ChartSliderWrapper from "./components/ChartSlider/ChartSliderWrapper";
-import FormikWizardWrapper from "./components/WizzardForm/FormikWizardWrapper";
-import { CssBaseline } from "@mui/material";
+import TabelWrapper from "./components/TabelWrapper/TabelWrapper";
+import FormikWizardWrapper, { FormikWizardWrapperProp } from "./components/WizzardForm/FormikWizardWrapper";
 
 const darkTheme = createTheme({
   palette: {
@@ -17,10 +18,30 @@ const darkTheme = createTheme({
 });
 
 const queryClient = new QueryClient();
-const symbol = "META";
 
 const Layot = () => {
   return <Outlet />;
+};
+
+const formikWizardWrapperProp: FormikWizardWrapperProp = {
+  formSteps: [
+    {
+      step: { component: () => <TabelWrapper symbol="META" metricsType={MetricsType.General} /> },
+      stepName: "META - General",
+    },
+    {
+      step: { component: () => <ChartSliderWrapper symbol="META" ratioType={RatioType.PE} /> },
+      stepName: "META - PE",
+    },
+    {
+      step: { component: () => <TabelWrapper symbol="AAPL" metricsType={MetricsType.General} /> },
+      stepName: "AAPL - General",
+    },
+    {
+      step: { component: () => <ChartSliderWrapper symbol="AAPL" ratioType={RatioType.PE} /> },
+      stepName: "AAPL - PE",
+    },
+  ],
 };
 
 const App = observer(() => {
@@ -32,7 +53,7 @@ const App = observer(() => {
           <Routes>
             <Route path="/" element={<Layot />}>
               <Route path="input" element={<InputSymbolPage />} />
-              <Route path="form" element={<FormikWizardWrapper />} />
+              <Route path="form" element={<FormikWizardWrapper formSteps={formikWizardWrapperProp.formSteps} />} />
               <Route path="*" element={<div />} />
             </Route>
           </Routes>
--- FrontEnd/src/components/WizzardForm/FormikWizardWrapper.tsx
+++ FrontEnd/src/components/WizzardForm/FormikWizardWrapper.tsx
@@ -1,37 +1,49 @@
 import Button from "@mui/material/Button";
-import { useFormikWizard } from "formik-wizard-form";
-import MetricsType from "../../Entity/MetricsType";
-import RatioType from "../../Entity/RatioType";
-import ChartSliderWrapper from "../ChartSlider/ChartSliderWrapper";
-import TabelWrapper from "../TabelWrapper/TabelWrapper";
+import StepMui from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+import Stepper from "@mui/material/Stepper";
+import { Step, useFormikWizard } from "formik-wizard-form";
+import { useState } from "react";
 import "./FormikWizardWrapper.css";
 
-const FormikWizardWrapper = () => {
-  const { renderComponent, handlePrev, handleNext, isNextDisabled, isPrevDisabled, isLastStep } = useFormikWizard({
-    initialValues: { firstName: "", lastName: "" },
-    onSubmit: (values: any) => console.log(values),
-    validateOnNext: true,
-    activeStepIndex: 0,
-    steps: [
-      {
-        component: () => <TabelWrapper symbol="META" metricsType={MetricsType.General} />,
-      },
-      {
-        component: () => <ChartSliderWrapper symbol="META" ratioType={RatioType.PE} />,
-      },
-      {
-        component: () => <TabelWrapper symbol="META" metricsType={MetricsType.CashFlow} />,
-      },
-    ],
-  });
+export type FormikWizardWrapperProp = {
+  formSteps: FormStep[];
+};
+
+type FormStep = {
+  step: Step;
+  stepName: string;
+};
+
+const FormikWizardWrapper = ({ formSteps }: FormikWizardWrapperProp) => {
+  const [finished, setFinished] = useState(false);
+
+  //create steps from formSteps prop and add type of the varibale
+  const steps: Step[] = formSteps.map((formStep) => formStep.step);
+
+  const { renderComponent, handlePrev, handleNext, isNextDisabled, isPrevDisabled, isLastStep, currentStepIndex } =
+    useFormikWizard({
+      initialValues: { firstName: "", lastName: "" },
+      onSubmit: (values: any) => setFinished(true),
+      validateOnNext: true,
+      activeStepIndex: 0,
+      steps: steps,
+    });
 
   return (
     <div className="form">
+      <Stepper activeStep={currentStepIndex}>
+        {formSteps.map((step, index) => (
+          <StepMui key={step.stepName} completed={formSteps.length - 1 !== index ? currentStepIndex > index : finished}>
+            <StepLabel>{step.stepName}</StepLabel>
+          </StepMui>
+        ))}
+      </Stepper>
       <div className="circle" style={{ filter: `blur(${((400 * 50) % 600) + 30}px)` }} />
       <div className="render-component">{renderComponent()}</div>
       <div className="buttons">
         <div className="previous-button-area">
-          <Button color="primary" size="large" disabled={isNextDisabled} onClick={handlePrev}>
+          <Button color="primary" size="large" disabled={isPrevDisabled} onClick={handlePrev}>
             Previous
           </Button>
         </div>
