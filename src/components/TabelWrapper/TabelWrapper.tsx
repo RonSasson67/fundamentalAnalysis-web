5b7ade0ea04381b4e8f54077d3c6463ab3ae1233
ron sason <yafagame@gmail.com>
2023-07-16 23:04:08 +0300

    Remove small component from tabel Wrapper

--- src/components/TabelWrapper/TabelWrapper.tsx
+++ src/components/TabelWrapper/TabelWrapper.tsx
@@ -1,9 +1,13 @@
+import GppBadIcon from "@mui/icons-material/GppBad";
+import GppGoodIcon from "@mui/icons-material/GppGood";
+import { TextField } from "@mui/material";
+import Checkbox from "@mui/material/Checkbox";
 import { FinancialData } from "../../Entity/FinancialData.interface.ts.ts";
 import MetricsType from "../../Entity/MetricsType.tsx";
 import CheckBoxStore from "../../Store/CheckBoxStore.tsx";
 import TextInputStore from "../../Store/TextInputStore.tsx";
+import ScrollBar from "../../Utils/Style/ScrollBar.tsx";
 import { useGetMetricData } from "../../api/MetricsApi.tsx";
-import BoxList from "./Component/BoxList.tsx";
 import "./TabelWrapper.css";
 
 interface TableWrapperProps {
@@ -30,7 +34,27 @@ function TableWrapper({ symbol, metricsType }: TableWrapperProps) {
       <h1 className="tabel-title">
         {metricsType} - {symbol}
       </h1>
-      <BoxList financialData={data as FinancialData[]} checkBoxStore={checkBoxStore} textInputStore={textInputStore} />
+      <div className={"list-container scroll-bar"}>
+        {(data as FinancialData[]).map((entry) => (
+          <div className="box-content">
+            <p className="box-title box-text">{entry.title}</p>
+            <p className="box-text">{entry.value}</p>
+            <Checkbox
+              icon={<GppBadIcon />}
+              checkedIcon={<GppGoodIcon />}
+              checked={checkBoxStore.isChecked(entry.title)}
+              onChange={() => checkBoxStore.toggle(entry.title)}
+            />
+            <TextField
+              label="Note"
+              variant="standard"
+              id="standard-basic"
+              key={entry.title}
+              onChange={(e) => textInputStore.updateInput(entry.title, e.target.value)}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
